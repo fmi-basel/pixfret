72ea03de1564ceb287b1810d8b589426a62913e2
Jan Eglinger <jan.eglinger@fmi.ch>
2019-11-22 15:39:14 +0100

    Travis: remove GPG signing

--- .travis/build.sh
+++ .travis/build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 curl -fsLO https://raw.githubusercontent.com/scijava/scijava-scripts/master/travis-build.sh
-sh travis-build.sh $encrypted_f8686d8bf72e_key $encrypted_f8686d8bf72e_iv
+sh travis-build.sh
